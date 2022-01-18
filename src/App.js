781dc9036535ccf722190ccbf224478f36974944
Rahul <kumrahulsingh7575@gmail.com>
2022-01-19 02:24:01 +0530

    Now deploy

--- src/App.js
+++ src/App.js
@@ -1,19 +1,70 @@
 
+import { useState } from 'react';
 import './App.css';
 // import About from './Components/About';
 import Navbar from './Components/Navbar';
 import Textform from './Components/Textform';
+import Alert from './Components/Alert';
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route
+// } from "react-router-dom";
 
 
 function App() {
+  const[mode,setdarkmode]=useState('light');
+  const [alert, setAlert] = useState(null);
+  const showAlert=(message,type)=>{
+    setAlert({
+      message:message,
+      type:type
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 2000);
+  }  
+ const toggle=()=>{
+   if(mode=='light'){
+    setdarkmode('dark');
+    document.body.style.backgroundColor='#0f0e0e';
+    showAlert("Dark mode has been enabled",'success');
+   }else{
+     setdarkmode('light');
+     document.body.style.backgroundColor='white';
+     showAlert("Light mode has been enabled",'success');
+   }
+  }
+ const toggle1=()=>{
+   if(mode=='light'){
+    setdarkmode('dark');
+    document.body.style.backgroundColor='green';
+    showAlert("Green mode has been enabled",'success');
+   }else{
+     setdarkmode('light');
+     document.body.style.backgroundColor='white';
+     showAlert("Light mode has been enabled",'success');
+   }
+  
+
+  }
   return (
     <>
-    
-    <Navbar title="TextUtils" about="About"/>
+    {/* <Router> */}
+    <Navbar title="Word-counter" toggle1={toggle1} mode={mode}  toggle={toggle}/>
+    <Alert alert={alert}/>
     <div className="container my-3">
-    <Textform heading="Enter the Text"/>
-    {/* <About/> */}
-    </div>
+    {/* <Switch>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route exact path="/">
+
+          </Route>
+        </Switch>
+      </Router> */}
+           <Textform showAlert={showAlert}  heading="Enter the Text" mode={mode}/>
+      </div>
 
      </>
   );
--- src/Components/Textform.js
+++ src/Components/Textform.js
@@ -1,38 +1,21 @@
 import React,{useState} from 'react'
 
 export default function Textform(props) {
-     const [btntext,setbtntext] = useState('Enable dark mode');
-     const [mystyle,setStyle] = useState({
-       color:'black',
-       backgroundColor:'white'
-     })
-     let toggle=()=>{
-       if(mystyle.color=='black'){
-         setStyle({
-         color: 'white',
-         backgroundColor:'black'
-         })
-         setbtntext('Disable dark mode');
-       }else{
-        setStyle({
-          color: 'black',
-          backgroundColor:'white'
-          })
-          setbtntext('Enable dark mode');
-       }
-     }
     const[text,setText]=useState('');
     const upcase= ()=>{
       let newText=text.toUpperCase();
       setText(newText);  
+      props.showAlert("Converted to upper case",'success');
     }
     const lowcase= ()=>{
       let newText=text.toLowerCase();
       setText(newText);  
+      props.showAlert("Converted to lower case",'success');
     }
     const del=()=>{
       let newText="";
       setText(newText);
+      props.showAlert("Text cleared",'success');
     }
     const title=()=>{
       let regex=/.([0-9]){10}/;
@@ -40,6 +23,7 @@ export default function Textform(props) {
       const res=digits.join("");
       let rem=res.slice(1,11);
       setText(rem);
+      props.showAlert("Your phone number is extracted",'success');
 
     }
     
@@ -48,33 +32,33 @@ export default function Textform(props) {
     }
     return (
       <>
-        <div className='container' style={mystyle}>
+        <div className='container'  style={{color: props.mode==='dark'?'white':'black'}}>
             <h1>{props.heading}</h1>
-      <div className="mb-3" style={mystyle}>
-        <textarea className="form-control my-3" value={text} style={mystyle}  onChange={handle} id="mybox" rows="7"></textarea>
+      <div className="mb-3" >
+        <textarea className="form-control my-3" value={text}   onChange={handle} style={{backgroundColor: props.mode==='dark'?'grey':'white',color: props.mode==='dark'?'white':'black'}} id="mybox" rows="6"></textarea>
       </div>
       <div className="dropdown">
   <button className="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-    Dropdown button
+    More functions
   </button>
   <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
     <li><a className="dropdown-item" onClick={upcase}href="#">Convert to Uppercase</a></li>
     <li><a className="dropdown-item" href="#" onClick={lowcase} >Convert to Lowercase</a></li>
     {/* <li><a className="dropdown-item" href="#" onClick={del}>Delete</a></li> */}
-    <li><a className="dropdown-item" href="#" onClick={title}>Extract Phone Number</a></li>
+    {/* <li><a className="dropdown-item" href="#" onClick={title}>Extract Phone Number</a></li> */}
   </ul>
-  <button className='btn btn-primary mx-3' onClick={toggle}>{btntext}</button>
+  <button className='btn btn-primary mx-3' onClick={title}>Extract Phone Number</button>
   <button className='btn btn-primary mx-3' onClick={del}>Delete</button>
 </div>
      
     
       </div>
-      <div className="container" style={mystyle}>
-        <h2>Your Text Summary</h2>
-        <p>{text.split(" ").length-1} words,{text.length} characters</p>
-        <p>It will take approx {0.008 * text.split(" ").length} minutes to read </p>
+      <div className="container my-2" style={{color: props.mode==='dark'?'white':'black'}} >
+        {/* <h2>Your Text Summary</h2> */}
+        <p><h2>{text.split(" ").length-1} words,{text.length} characters</h2></p>
+        <p>It will take approx {(0.008 * text.split(" ").length-0.008).toFixed(3)} minutes to read </p>
         <h2>Preview</h2>
-        <p>{text}</p>
+        <p>{text.length>0?text:"Enter something in above textbox to preview it here"}</p>
       </div>
       </>
     )
